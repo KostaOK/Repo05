3c830821b500a6ec604ca4060861d760b6cb47e2
Niko Kostič <kostic.niko@icloud.com>
2022-04-01 10:29:35 +0200

    Constructing Strings w Variables

---
+++ ex07/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    // only change code above this line
+    var myName="Nikolaj";
+    var myString="My name is "+myName+" and I am well!";
+    // only change code above this line
+    return myString;
+}
+
+console.log(main());
+module.exports=main;

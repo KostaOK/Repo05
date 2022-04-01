ab9ca82a528b52d292ff22f975da6cdfb677a151
Niko Kostič <kostic.niko@icloud.com>
2022-04-01 23:23:45 +0200

    Bracket Notation to find Nth to Last Character

---
+++ ex14/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    // only change code above this line
+    var lastName="Bezos";
+    var thirdToLastLetterofLastName=lastName[lastName.length-3];
+    // only change code above this line
+    return thirdToLastLetterofLastName;
+}
+
+console.log(main());
+module.exports=main;

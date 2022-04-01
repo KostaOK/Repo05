694543f36cde48a9fb55656d842ef46a6034fa06
Niko Kostič <kostic.niko@icloud.com>
2022-04-01 10:15:42 +0200

    Concatenating Strings w + Operator

---
+++ ex05/src/index.js
@@ -0,0 +1,9 @@
+function main(){
+    // only change code above this line
+    var myString="First string. "+"Second string.";
+    // only change code above this line
+    return myString;
+}
+
+console.log(main());
+module.exports=main;

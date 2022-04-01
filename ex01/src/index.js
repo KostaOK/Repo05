82401c7209a2e2f567ee62688f562867c8e53b49
Niko Kostič <kostic.niko@icloud.com>
2022-04-01 09:16:59 +0200

    Declare String Variables

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    // only change code above this line
+    var myFirstName="Niko";
+    var myLastName="Kostič";
+    myHomeTown="Maribor";
+    // only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main())
+module.exports=main;

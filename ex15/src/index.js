9622eace5e742202f514b1ecd08e15c3f7ceb632
Niko Kostič <kostic.niko@icloud.com>
2022-04-02 00:17:21 +0200

    Word Blanks

---
+++ ex15/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    // only change code above this line
+    var myNoun="dog";
+    var myVerb="ran";
+    var myAdjective="big";
+    var myAdverb="quickly";
+    var wordBlanks="";
+    wordBlanks="My "+ myNoun + " is "+ myAdjective + " and he " + myVerb + " very " + myAdverb + ".";
+    // only change code above this line
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports=main;

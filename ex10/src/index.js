20a65abb555c58643af994419a529d9dadefbcf0
Niko Kostič <kostic.niko@icloud.com>
2022-04-01 11:09:55 +0200

    Bracket Notation to find 1st Character in a String

---
+++ ex10/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    // only change code above this line
+    var lastName="Doe";
+    var firstLetterofLastName=" ";
+    var firstLetterofLastName=lastName[0];
+    // only change code above this line
+    return firstLetterofLastName;
+}
+
+console.log(main());
+module.exports=main;

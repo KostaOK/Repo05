5050089e39beb4f018dca2ec0c4a6c2d2623e34f
Niko Kostič <kostic.niko@icloud.com>
2022-04-01 23:07:11 +0200

    Bracket Notation to find Nth character in a String

---
+++ ex12/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    // only change code above this line
+    var lastName="Tesla";
+    var thirdLetterofLastName=lastName[2];
+    // only change code above this line
+    return thirdLetterofLastName;
+}
+
+console.log(main());
+module.exports=main;

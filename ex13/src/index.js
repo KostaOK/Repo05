2d2204b4971af730bdc2beaeec017d80a07e9325
Niko Kostič <kostic.niko@icloud.com>
2022-04-01 23:18:27 +0200

    Bracket Notation to find Last Character

---
+++ ex13/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    // only change code above this line
+    var lastName="Tesla";
+    var lastLetterofLastName=lastName[lastName.length-1];
+    // only change code above this line
+    return lastLetterofLastName;
+}
+
+console.log(main());
+module.exports=main;

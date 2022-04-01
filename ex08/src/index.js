a813f900856463ef542a39f1d7ea9c1c9de77413
Niko Kostič <kostic.niko@icloud.com>
2022-04-01 10:33:52 +0200

    Appending Variables to Strings

---
+++ ex08/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    // only change code above this line
+    var someAdjective="the best!";
+    var myString="Mistral is ";
+    myString+=someAdjective;
+    // only change code above this line
+    return myString;
+}
+
+console.log(main());
+module.exports=main;

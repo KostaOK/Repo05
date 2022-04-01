691c21e216a63f12e082f0533eb5b1f528dd3691
Niko Kostič <kostic.niko@icloud.com>
2022-04-01 10:49:55 +0200

    Length of a String

---
+++ ex09/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    // only change code above this line
+    var lastName="Doe";
+    var lastNameLength;
+    lastNamelength;
+    // only change code above this line
+    return lastNameLength;
+}
+
+console.log(main());
+module.exports=main;
